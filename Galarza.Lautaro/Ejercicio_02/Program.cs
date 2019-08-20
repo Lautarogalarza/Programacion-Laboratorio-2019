6a7c9baf74689aa1b11cff658abcc62e63038f56
Lautaro Galarza <40771990+Lautarogalarza@users.noreply.github.com>
2019-08-20 12:08:46 -0300

    Add files via upload

---
+++ Galarza.Lautaro/Ejercicio_02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int maximo;
+            int minimo;
+            float promedio;
+            int i;
+
+              Console.WriteLine("Ingrese un numero;");
+            for(i=0;i>5;i++)
+            {
+
+                numero= int.Parse( Console.ReadLine() );
+
+                Console.WriteLine(" {0}  ", numero);
+
+
+            }
+                Console.ReadLine();
+           
+           
+                
+
+       
+
+        }
+    }
+}
